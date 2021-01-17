028da4154c61203eabb16225d14a0299f9952bb0
Vipra Dwivedi <vipradwivedi@users.noreply.github.com>
2021-01-17 11:28:38 +0530

    Update and rename sipi.py to log_parser_split.py

---
+++ log_parser_split.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+'''
+This script takes input log filename
+
+$ ./log_parser_split.py sample_logs.txt
+'''
+
+import sys
+from collections import Counter
+
+
+def apache_output(line):
+    split_line = line.split()
+    return {
+        'status': split_line[8],
+    }
+
+
+def final_report(logfile):
+    status_codes = Counter()
+    for line in logfile:
+        line_dict = apache_output(line)
+        print(line_dict)
+
+        status_codes.update({line_dict['status']: 1})
+
+    print(status_codes)
+    print("Tops 2 status codes: {}".format(status_codes.most_common(2)))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 1:
+        print(__doc__)
+        sys.exit(1)
+    infile_name = sys.argv[1]
+try:
+    infile = open(infile_name, 'r')
+except IOError:
+    print("You must specify a valid file to parse")
+    print(__doc__)
+    sys.exit(1)
+
+final_report(infile)
+infile.close()
+
+
+# output
+# $ ./log_parser_split.py sample_logs.txt
+# {'status': '200'}
+# {'status': '201'}
+# {'status': '202'}
+# {'status': '400'}
+# {'status': '201'}
+# {'status': '200'}
+# Counter({'201': 2, '200': 2, '202': 1, '400': 1})
+# Tops 2 status codes: [('201', 2), ('200', 2)]
